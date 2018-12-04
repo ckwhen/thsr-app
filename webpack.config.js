06556e83fb450d007e13f7fc029a0ad459ce364c
Chih Kai Wen <ckwen1988@gmail.com>
2018-12-04 13:32:29 +0800

    環境建置

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const host = 'localhost';
+const port = 8081;
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      }
+    ]
+  },
+  devServer: {
+    host,
+    port,
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      inject: true,
+    })
+  ],
+};
