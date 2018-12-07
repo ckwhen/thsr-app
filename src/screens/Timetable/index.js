c1fa7c1438118a6557cece825e51733aa1fe3b75
Chih Kai Wen <ckwen1988@gmail.com>
2018-12-07 12:49:38 +0800

    完成timetable功能

--- src/screens/Timetable/index.js
+++ src/screens/Timetable/index.js
@@ -1,12 +1,155 @@
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Selector from '../../components/Selector';
+import {
+  setOriginStation,
+  setDestinationStation,
+  fetchStations,
+  loadTimetable,
+} from './reducers';
+
+const propTypes = {
+  loadTimetable: PropTypes.func,
+  setOriginStation: PropTypes.func,
+  setDestinationStation: PropTypes.func,
+  fetchStations: PropTypes.func,
+  originStationID: PropTypes.string,
+  destinationStationID: PropTypes.string,
+  trainDate: PropTypes.any,
+  stations: PropTypes.array,
+  timetable: PropTypes.arrayOf(PropTypes.shape({
+    trainNo: PropTypes.string,
+    trainDate: PropTypes.string,
+    direction: PropTypes.number,
+    originStationName: PropTypes.object,
+    originDepartureTime: PropTypes.string,
+    destinationStationName: PropTypes.object,
+    destinationArrivalTime: PropTypes.string,
+    totalTime: PropTypes.string,
+    fare: PropTypes.number,
+    isBusinessSeat: PropTypes.bool,
+  })),
+};
+
+const mapStateToProps = state => state.timetable;
+const mapDispatchToProps = {
+  setOriginStation,
+  setDestinationStation,
+  fetchStations,
+  loadTimetable,
+};
 
 class Timetable extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  static get propTypes() {
+    return propTypes;
+  }
+
+  componentDidMount() {
+    this.props.fetchStations()
+      .then((response) => {
+        // 設定 search bar 預設為第一筆車站資料
+        if (response && response.length) {
+          const firstStation = response[0];
+          this.props.setOriginStation(firstStation.stationID);
+          this.props.setDestinationStation(firstStation.stationID);
+        }
+      });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const {
+      originStationID,
+      destinationStationID,
+    } = this.props;
+    return this.props.loadTimetable(originStationID, destinationStationID);
+  }
+
   render() {
+    const {
+      originStationID,
+      destinationStationID,
+      stations,
+      timetable,
+    } = this.props;
     return (
-      <div>timetable</div>
+      <div>
+        <div>
+          <form className="form-inline" onSubmit={this.handleSubmit}>
+            <Selector
+              id="originStationID"
+              name="originStationID"
+              value={originStationID}
+              options={stations}
+              onChange={event => this.props.setOriginStation(event.target.value)}
+              labelRenderer={option => option.stationName.zhTw}
+              valueRenderer={option => option.stationID}
+            />
+            <Selector
+              id="destinationStationID"
+              name="destinationStationID"
+              value={destinationStationID}
+              options={stations}
+              onChange={event => this.props.setDestinationStation(event.target.value)}
+              labelRenderer={option => option.stationName.zhTw}
+              valueRenderer={option => option.stationID}
+            />
+            <button className="btn btn-primary mb-2" type="submit">Submit form</button>
+          </form>
+        </div>
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">起站名稱</th>
+                <th scope="col">起站發車時間</th>
+                <th scope="col">訖站名稱</th>
+                <th scope="col">訖站到達時間</th>
+                <th scope="col">總共乘車時間</th>
+                <th scope="col">票價</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                timetable && timetable.map((time) => {
+                  const {
+                    trainNo,
+                    trainDate,
+                    originStationName,
+                    originDepartureTime,
+                    destinationStationName,
+                    destinationArrivalTime,
+                    totalTime,
+                    fare,
+                    isBusinessSeat,
+                  } = time;
+                  return (
+                    <tr key={`${trainNo}-${trainDate}-${originDepartureTime}`}>
+                      <td>{originStationName.zhTw}</td>
+                      <td>{originDepartureTime}</td>
+                      <td>{destinationStationName.zhTw}</td>
+                      <td>{destinationArrivalTime}</td>
+                      <td>{totalTime}</td>
+                      <td>
+                        {fare}
+                        {isBusinessSeat && <span title={'此價格為商務車箱票價'}>i</span>}
+                      </td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
   }
 }
 
-export default Timetable;
+export default connect(mapStateToProps, mapDispatchToProps)(Timetable);
--- src/screens/Timetable/reducers/index.js
+++ src/screens/Timetable/reducers/index.js
@@ -1,5 +1,272 @@
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+import { keyBy } from 'lodash';
+import { CALL_API } from '../../../middlewares/api';
+import { THSR_API_SERVER } from '../../../configs';
+import {
+  DATE_FORMAT,
+  TIME_FORMAT,
+} from '../../../constants';
+import {
+  hasAvailableSeat,
+  noAvailableSeat,
+  isRegularTicket,
+  isBusinessTicket,
+} from '../../../utils';
+
+momentDurationFormatSetup(moment);
+
+const PATH = 'thsr-app/timetable/';
+const FETCH_STATIONS_REQUEST = `${PATH}FETCH_STATIONS_REQUEST`;
+const FETCH_STATIONS_SUCCESS = `${PATH}FETCH_STATIONS_SUCCESS`;
+const FETCH_STATIONS_FAILURE = `${PATH}FETCH_STATIONS_FAILURE`;
+const FETCH_DAILY_TIMETABLE_REQUEST = `${PATH}FETCH_DAILY_TIMETABLE_REQUEST`;
+const FETCH_DAILY_TIMETABLE_SUCCESS = `${PATH}FETCH_DAILY_TIMETABLE_SUCCESS`;
+const FETCH_DAILY_TIMETABLE_FAILURE = `${PATH}FETCH_DAILY_TIMETABLE_FAILURE`;
+const FETCH_OD_FARE_REQUEST = `${PATH}FETCH_OD_FARE_REQUEST`;
+const FETCH_OD_FARE_SUCCESS = `${PATH}FETCH_OD_FARE_SUCCESS`;
+const FETCH_OD_FARE_FAILURE = `${PATH}FETCH_OD_FARE_FAILURE`;
+const FETCH_AVAILABLE_SEAT_STATUS_LIST_REQUEST = `${PATH}FETCH_AVAILABLE_SEAT_STATUS_LIST_REQUEST`;
+const FETCH_AVAILABLE_SEAT_STATUS_LIST_SUCCESS = `${PATH}FETCH_AVAILABLE_SEAT_STATUS_LIST_SUCCESS`;
+const FETCH_AVAILABLE_SEAT_STATUS_LIST_FAILURE = `${PATH}FETCH_AVAILABLE_SEAT_STATUS_LIST_FAILURE`;
+const LOAD_TIMETABLE_REQUEST = `${PATH}LOAD_TIMETABLE_REQUEST`;
+const LOAD_TIMETABLE_SUCCESS = `${PATH}LOAD_TIMETABLE_SUCCESS`;
+const LOAD_TIMETABLE_FAILURE = `${PATH}LOAD_TIMETABLE_FAILURE`;
+const SET_ORIGIN_STATION = `${PATH}SET_ORIGIN_STATION`;
+const SET_DESTINATION_STATION = `${PATH}SET_DESTINATION_STATION`;
+
 const initialState = {
+  todayObj: moment(),
+  originStationID: '',
+  destinationStationID: '',
+  isStationsFetched: false,
+  stations: [],
+  isTimetableFetched: false,
+  timetable: [],
 };
-export default function reducer(state = initialState) {
-  return state;
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_STATIONS_REQUEST:
+      return Object.assign({}, state, {
+        isStationsFetched: false,
+      });
+    case FETCH_STATIONS_SUCCESS:
+      return Object.assign({}, state, {
+        isStationsFetched: true,
+        stations: action.response,
+      });
+    case FETCH_STATIONS_FAILURE:
+      return Object.assign({}, state, {
+        isStationsFetched: true,
+      });
+    case LOAD_TIMETABLE_REQUEST:
+      return Object.assign({}, state, {
+        isTimetableFetched: false,
+      });
+    case LOAD_TIMETABLE_SUCCESS:
+      return Object.assign({}, state, {
+        isTimetableFetched: true,
+        timetable: action.payload,
+      });
+    case LOAD_TIMETABLE_FAILURE:
+      return Object.assign({}, state, {
+        isTimetableFetched: true,
+      });
+    case SET_ORIGIN_STATION:
+    case SET_DESTINATION_STATION:
+      return Object.assign({}, state, {
+        [action.payload.valueName]: action.payload.value,
+      });
+    default:
+      return state;
+  }
+}
+
+function setSearchQuery(actionType, valueName, value) {
+  return {
+    type: actionType,
+    payload: {
+      valueName,
+      value,
+    },
+  };
+}
+
+export function setOriginStation(value) {
+  return setSearchQuery(SET_ORIGIN_STATION, 'originStationID', value);
+}
+
+export function setDestinationStation(value) {
+  return setSearchQuery(SET_DESTINATION_STATION, 'destinationStationID', value);
+}
+
+// 取得車站資訊
+export function fetchStations() {
+  return {
+    [CALL_API]: {
+      url: `${THSR_API_SERVER}Station`,
+      params: {
+        $top: 30,
+        $format: 'JSON',
+      },
+      types: [
+        FETCH_STATIONS_REQUEST,
+        FETCH_STATIONS_SUCCESS,
+        FETCH_STATIONS_FAILURE,
+      ],
+    },
+  };
+}
+
+// 取得該日起迄站時刻表
+export function fetchDailyTimetable(originStationID, destinationStationID, trainDate) {
+  return {
+    [CALL_API]: {
+      url: `${THSR_API_SERVER}DailyTimetable/OD/${originStationID}/to/${destinationStationID}/${trainDate}`,
+      params: {
+        $orderby: 'OriginStopTime/DepartureTime',
+      },
+      types: [
+        FETCH_DAILY_TIMETABLE_REQUEST,
+        FETCH_DAILY_TIMETABLE_SUCCESS,
+        FETCH_DAILY_TIMETABLE_FAILURE,
+      ],
+    },
+  };
+}
+
+// 取得起迄站票價
+export function fetchODFare(originStationID, destinationStationID) {
+  return {
+    [CALL_API]: {
+      url: `${THSR_API_SERVER}ODFare/${originStationID}/to/${destinationStationID}`,
+      types: [
+        FETCH_OD_FARE_REQUEST,
+        FETCH_OD_FARE_SUCCESS,
+        FETCH_OD_FARE_FAILURE,
+      ],
+    },
+  };
+}
+
+// 取得動態指定車站對號座剩餘座位
+export function fetchAvailableSeatStatusList(stationID) {
+  return {
+    [CALL_API]: {
+      url: `${THSR_API_SERVER}AvailableSeatStatusList/${stationID}`,
+      types: [
+        FETCH_AVAILABLE_SEAT_STATUS_LIST_REQUEST,
+        FETCH_AVAILABLE_SEAT_STATUS_LIST_SUCCESS,
+        FETCH_AVAILABLE_SEAT_STATUS_LIST_FAILURE,
+      ],
+    },
+  };
+}
+
+// 取得時刻表表格資料
+export function loadTimetable(originStationID, destinationStationID) {
+  return (dispatch, getState) => {
+    const { todayObj } = getState().timetable;
+    dispatch({ type: LOAD_TIMETABLE_REQUEST });
+    return Promise.all([
+      dispatch(fetchDailyTimetable(
+        originStationID,
+        destinationStationID,
+        todayObj.format(DATE_FORMAT),
+      )),
+      dispatch(fetchODFare(originStationID, destinationStationID)),
+      dispatch(fetchAvailableSeatStatusList(originStationID)),
+    ])
+      .then((response) => {
+        const [dailyTimetable, odFare, availableSeatStatusList] = response;
+        const fares = (odFare && odFare.length && odFare[0].fares) || [];
+        let availableSeatHash = {};
+        if (
+          availableSeatStatusList
+          && availableSeatStatusList.length
+        ) {
+          availableSeatHash = keyBy(availableSeatStatusList[0].availableSeats, 'trainNo');
+        }
+
+        const timetable = dailyTimetable
+          .map((time) => {
+            const {
+              trainDate,
+              originStopTime,
+              destinationStopTime,
+              dailyTrainInfo: {
+                trainNo,
+                direction,
+              },
+            } = time;
+            const originDepartureTime = moment(originStopTime.departureTime, TIME_FORMAT);
+            const destinationArrivalTime = moment(destinationStopTime.arrivalTime, TIME_FORMAT);
+
+            // 取得迄站的剩餘座位資料
+            const trainWithAvailableSeat = availableSeatHash[trainNo];
+            let stopStation = {};
+            if (
+              trainWithAvailableSeat
+              && trainWithAvailableSeat.stopStations
+              && trainWithAvailableSeat.stopStations.length
+            ) {
+              stopStation = trainWithAvailableSeat.stopStations
+                .find(station => station.stationID === destinationStopTime.stationID);
+            }
+            const {
+              businessSeatStatus,
+              standardSeatStatus,
+            } = stopStation;
+
+            // 取得總乘車時間
+            const totalTime = moment
+              .duration(destinationArrivalTime.diff(originDepartureTime), 'milliseconds')
+              .format(TIME_FORMAT, { trim: false });
+
+            // 取得票價
+            let fare = 0;
+            let isBusinessSeat = false;
+            if (hasAvailableSeat(standardSeatStatus)) {
+              fare = fares.find(item => isRegularTicket(item.ticketType)).price;
+            } else if (hasAvailableSeat(businessSeatStatus)) {
+              isBusinessSeat = true;
+              fare = fares.find(item => isBusinessTicket(item.ticketType)).price;
+            }
+
+            // 過濾條件
+            if (
+              // 排除尚未發車的車次
+              originDepartureTime.isSameOrBefore(todayObj)
+              // 排除標準車箱與商務車箱皆已滿坐的車次
+              || (noAvailableSeat(businessSeatStatus) && noAvailableSeat(standardSeatStatus))
+            ) {
+              return null;
+            }
+            return Object.assign({}, {
+              trainNo,
+              trainDate,
+              direction,
+              totalTime,
+              fare,
+              isBusinessSeat,
+              originStationName: originStopTime.stationName,
+              originDepartureTime: originStopTime.departureTime,
+              destinationStationName: destinationStopTime.stationName,
+              destinationArrivalTime: destinationStopTime.arrivalTime,
+            });
+          })
+          // 排除不符合過濾條件的車次
+          .filter(time => time);
+
+        dispatch({
+          type: LOAD_TIMETABLE_SUCCESS,
+          payload: timetable,
+        });
+        return Promise.resolve(response);
+      })
+      .catch((error) => {
+        dispatch({ type: LOAD_TIMETABLE_FAILURE });
+        return Promise.reject(error);
+      });
+  };
 }
